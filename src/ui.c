5b3f99fe1bd895e96a195757fb8beed26f3f80e6
Evgeniy <ewginx@gmail.com>
2023-05-09 02:22:23 +1000

    create init function for dock

--- src/ui.c
+++ src/ui.c
@@ -719,24 +719,6 @@ void ui_DigitalClock_screen_init(void)
     lv_obj_set_align(ui_DigitalClockPanel, LV_ALIGN_TOP_LEFT);
     lv_obj_clear_flag(ui_DigitalClockPanel, LV_OBJ_FLAG_SCROLLABLE); /// Flags
 
-    ui_BatteryLabel = lv_label_create(ui_DigitalClockPanel);
-    lv_obj_set_pos(ui_BatteryLabel, 20, 0);
-    lv_obj_set_align(ui_BatteryLabel, LV_ALIGN_TOP_LEFT);
-    lv_obj_set_size(ui_BatteryLabel, 40, 30);
-    lv_label_set_text(ui_BatteryLabel, LV_SYMBOL_BATTERY_2);
-    lv_obj_set_style_text_font(ui_BatteryLabel, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-    ui_SettingsButton = lv_btn_create(ui_DigitalClockPanel);
-    lv_obj_set_size(ui_SettingsButton, 35, 35);
-    lv_obj_set_pos(ui_SettingsButton, 401, -6);
-    lv_obj_set_align(ui_SettingsButton, LV_ALIGN_TOP_LEFT);
-    lv_obj_set_style_bg_opa(ui_SettingsButton, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-    ui_SettingsButtonLabel = lv_label_create(ui_SettingsButton);
-    lv_obj_set_align(ui_SettingsButtonLabel, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_SettingsButtonLabel, LV_SYMBOL_SETTINGS);
-    lv_obj_set_style_text_align(ui_SettingsButtonLabel, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
-
     ui_DigitalClockLabel = lv_label_create(ui_DigitalClockPanel);
     lv_obj_set_size(ui_DigitalClockLabel, 320, 100);
     lv_obj_set_pos(ui_DigitalClockLabel, 70, 75);
@@ -762,7 +744,6 @@ void ui_DigitalClock_screen_init(void)
     lv_obj_set_style_text_font(ui_DigitalClockDateLabel, &lv_font_montserrat_32, LV_PART_MAIN | LV_STATE_DEFAULT);
 
     lv_obj_add_event_cb(ui_DigitalClockScreen, ui_event_DigitalClockScreen, LV_EVENT_ALL, NULL);
-    lv_obj_add_event_cb(ui_SettingsButton, ui_event_SettingsButton, LV_EVENT_ALL, NULL);
 }
 void ui_Weather_screen_init(void)
 {
@@ -867,7 +848,29 @@ void ui_Weather_screen_init(void)
 
     lv_obj_add_event_cb(ui_WeatherScreen, ui_event_WeatherScreen, LV_EVENT_ALL, NULL);
 }
+void ui_Dock_init()
+{
+    lv_obj_t *panel = lv_obj_get_child(lv_scr_act(), 0);
+    ui_BatteryLabel = lv_label_create(panel);
+    lv_obj_set_pos(ui_BatteryLabel, 20, 0);
+    lv_obj_set_align(ui_BatteryLabel, LV_ALIGN_TOP_LEFT);
+    lv_obj_set_size(ui_BatteryLabel, 40, 30);
+    lv_label_set_text(ui_BatteryLabel, LV_SYMBOL_BATTERY_2);
+    lv_obj_set_style_text_font(ui_BatteryLabel, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
 
+    ui_SettingsButton = lv_btn_create(panel);
+    lv_obj_set_size(ui_SettingsButton, 35, 35);
+    lv_obj_set_pos(ui_SettingsButton, 401, -6);
+    lv_obj_set_align(ui_SettingsButton, LV_ALIGN_TOP_LEFT);
+    lv_obj_set_style_bg_opa(ui_SettingsButton, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    ui_SettingsButtonLabel = lv_label_create(ui_SettingsButton);
+    lv_obj_set_align(ui_SettingsButtonLabel, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_SettingsButtonLabel, LV_SYMBOL_SETTINGS);
+    lv_obj_set_style_text_align(ui_SettingsButtonLabel, LV_TEXT_ALIGN_CENTER, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    lv_obj_add_event_cb(ui_SettingsButton, ui_event_SettingsButton, LV_EVENT_ALL, NULL);
+}
 void ui_init(void)
 {
     lv_disp_t *dispp = lv_disp_get_default();
@@ -881,4 +884,5 @@ void ui_init(void)
     ui_Weather_screen_init();
     // ui____initial_actions0 = lv_obj_create(NULL);
     lv_disp_load_scr(ui_DigitalClockScreen);
+    ui_Dock_init();
 }
